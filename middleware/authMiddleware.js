c5051e733c9547c7b643935a317100d500fa7b39
Mohammed Abdul Jabbar Khan <133209466+Qbecwizrd@users.noreply.github.com>
2025-07-02 21:42:30 +0530

    Add files via upload

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken')
+
+
+const authMiddleware = (req,res,next) => {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+
+    if (!token) return res.status(401).json({ message: 'No token, authorization denied' });
+
+    try{
+         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+         req.user = decoded; // payload attached during login
+         next();
+
+    }
+    catch(error){
+        console.error(error);
+        res.status(401).json({ message: 'Invalid token' });
+    }
+};
+
+module.exports = authMiddleware;
